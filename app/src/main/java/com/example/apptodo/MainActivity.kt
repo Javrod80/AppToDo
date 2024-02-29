f15baf56f867ef35ce396c0f8fbe34858bea2275
javrod80 <javrod80@gmail.com>
2024-02-29 13:59:31 +0100

    1º Commit

---
+++ app/src/main/java/com/example/apptodo/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.apptodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var db : DatabaseHelper = DatabaseHelper(this)
+
+
+    }
+}
